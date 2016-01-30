d94880f961fa9686d9aa4c539396b5702837d8b3
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-01-30 18:02:38 +0100

    Save windows position

--- edocuments/__init__.py
+++ edocuments/__init__.py
@@ -3,6 +3,7 @@
 import os
 import sys
 from yaml import load
+from PyQt5.QtCore import QSettings
 from PyQt5.QtWidgets import QApplication
 
 from edocuments.main_widget import MainWindow
@@ -18,19 +19,29 @@ else:
 
 config = {}
 root_folder = None
+settings = None
 
 
 def gui_main():
-    global config, root_folder
+    global config, root_folder, settings
     with open(CONFIG_PATH) as f:
         config = load(f.read())
     root_folder = "%s/%s/" % (
         os.path.expanduser('~'),
         config.get("root_folder"),
     )
-    print(root_folder)
+    settings = QSettings("org", "edocuments")
 
     app = QApplication(sys.argv)
     mw = MainWindow()
+    print (settings.value("geometry"))
+    if settings.value("geometry") is not None:
+        mw.restoreGeometry(settings.value("geometry"))
+    if settings.value("state") is not None:
+        mw.restoreState(settings.value("state"))
     mw.show()
     app.exec()
+    print (mw.saveGeometry())
+    settings.setValue("geometry", mw.saveGeometry())
+    settings.setValue("state", mw.saveState())
+    settings.sync()
