38f98460f4cae5f2d7be14082be7741dad373e01
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-11-22 20:09:10 +0100

    Execute the right command

--- edocuments/label_dialog.py
+++ edocuments/label_dialog.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QDialog, QPushButton
 from PyQt5.QtGui import QPixmap
 import edocuments
 from edocuments.ui.label_dialog import Ui_Dialog
+
+
+class _Cmd(QObject):
+    def __init__(self, dialog, cmd):
+        self.dialog = dialog
+        self.cmd = cmd
+        super(_Cmd, self).__init__(dialog)
+
+    def exec_(self):
+        filename, extension = self.dialog.process.process(
+            [self.cmd], filenames=[self.dialog.image],
+        )
+        self.dialog.set_image(filename)
 
 
 class Dialog(QDialog):
@@ -26,13 +40,9 @@ class Dialog(QDialog):
                 button = QPushButton(self)
                 button.setText(cmd["display"])
 
-                def action():
-                    filename, extension = self.process.process(
-                        [cmd], filenames=[self.image],
-                    )
-                    self.set_image(filename)
+                c = _Cmd(self, cmd)
 
-                button.clicked.connect(action)
+                button.clicked.connect(c.exec_)
                 self.ui.button_container.addWidget(button)
 
 
